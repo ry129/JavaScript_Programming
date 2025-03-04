1e45dcd39f6ff9d130cce685ad3b08a9d42cfcd4
Riya Yadav <114867989+ry129@users.noreply.github.com>
2025-03-04 21:22:05 +0530

    Delete Day1-JsProgrammingConstructs directory

--- Day1-JsProgrammingConstructs/SelectionPracticeProblem/LeapYear.js
+++
@@ -1,6 +0,0 @@
-let year = parseInt(process.argv[2], 10);
-if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-  console.log(year + " is a leap year");
-} else {
-  console.log(year + " is not a leap year");
-}
--- Day1-JsProgrammingConstructs/SelectionPracticeProblem/coinFlip.js
+++
@@ -1,2 +0,0 @@
-let coinflip = Math.floor(Math.random() * 2); // Generates 0 or 1
-console.log(coinflip === 0 ? "Heads" : "Tails");
